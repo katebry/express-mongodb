64ad34fc0902686c1a8d4dcf532ded42e83e7f79
Kate Bryan <katebryan31@gmail.com>
2021-03-24 14:54:47 +0000

    feat: added nodemon and run script to package.json

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 8080;
 
 app.use('/api', apiRoutes)
 
-app.get("/", (req, res) => res.send("Kate's Express Server!"));
+app.get("/", (req, res) => res.send("Kate's Express Server!!!!!"));
 
 app.listen(port, function () {
   console.log("Running Express Server on port: " + port);
