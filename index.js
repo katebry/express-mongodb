458f3d5a024f0c6ff8cac7fd1dc54e978eef2ccf
Kate Bryan <katebryan31@gmail.com>
2021-03-29 10:10:48 +0100

    chore: added CRUD methods to index to check functionality

--- index.js
+++ index.js
@@ -70,9 +70,38 @@ async function findAllBooks(client) {
 
 main().catch(console.error);
 
-app.use("/api", apiRoutes);
+// TODO, move these back into the Router file
+app.get('/', function(req, res) {
+  console.log(req.body)
+  res.json({
+      status: 'API Works',
+      message: 'First API Res'
+  });
+});
 
-app.get("/", (req, res) => res.send("Kate's Express Server!"));
+app.get('/books', function(req, res) {
+  console.log(req.body, ': /books request')
+  res.json({
+      status: '200',
+      message: 'GET allBooks'
+  });
+});
+
+app.get(`/books/:title`, function(req, res) {
+  console.log(req.body, ': /books/:title request')
+  res.json({
+      status: '200',
+      message: 'GET bookByTitle'
+  });
+});
+
+app.post('/addBook', function(req, res) {
+  console.log(req.body, ': /addBook request')
+  res.json({
+      status: '201',
+      message: 'POST addBook'
+  });
+});
 
 app.listen(MY_PORT, function () {
   console.log("Running Express Server on port: " + MY_PORT);
